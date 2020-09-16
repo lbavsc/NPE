d06740ec6588bfffa64412ff9db178865a4857bb
lbavsc <471242574@qq.com>
2020-09-16 21:52:42 +0800

    更换头像

--- app/src/main/java/com/experiment/npe/ui/main/fragment/TabBar2Fragment.java
+++ app/src/main/java/com/experiment/npe/ui/main/fragment/TabBar2Fragment.java
@@ -140,36 +140,6 @@ public class TabBar2Fragment extends BaseFragment<FragmentTabBar2Binding, TabBar
                 });
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // 回调成功
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-
-                // 小图切割
-                case REQUEST_SMALL_IMAGE_CUTTING:
-                    if (data != null) {
-                        setPicToView(data);
-                    }
-                    break;
-
-                // 相册选取
-                case REQUEST_IMAGE_GET:
-                    try {
-                        startSmallPhotoZoom(data.getData());
-                    } catch (NullPointerException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-
-                // 拍照
-                case REQUEST_IMAGE_CAPTURE:
-                    File temp = new File(Environment.getExternalStorageDirectory() + "/" + IMAGE_FILE_NAME);
-                    startSmallPhotoZoom(Uri.fromFile(temp));
-                    break;
-            }
-        }
-    }
 
     public void photoMenu() {
         binding.userIcon.setOnClickListener(new View.OnClickListener() {
@@ -179,13 +149,13 @@ public class TabBar2Fragment extends BaseFragment<FragmentTabBar2Binding, TabBar
                     @Override
                     public void onClick(View v) {
                         // 进入相册选择
-                        turnPhotoAlbum();
+
                     }
                 }, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         // 拍照
-                        turnCamera();
+
                     }
                 });
                 View rootView = LayoutInflater.from(TabBar2Fragment.this.getActivity()).inflate(R.layout.activity_main, null);
@@ -195,127 +165,5 @@ public class TabBar2Fragment extends BaseFragment<FragmentTabBar2Binding, TabBar
         });
     }
 
-    /**
-     * 打开相册
-     */
-    public void turnPhotoAlbum() {
-        mPhotoPopupWindow.dismiss();
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        // 判断系统中是否有处理该 Intent 的 Activity
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_GET);
-        } else {
-            ToastUtils.showShort("未找到图片查看器");
-        }
-    }
 
-    /**
-     * 打开相机
-     */
-    public void turnCamera(){
-        Intent intent;
-        Uri pictureUri;
-        File pictureFile = new File(Environment.getExternalStorageDirectory(), IMAGE_FILE_NAME);
-        // 判断当前系统
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            pictureUri = FileProvider.getUriForFile(this.getActivity(),
-                    "com.experiment.npe", pictureFile);
-        } else {
-            intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            pictureUri = Uri.fromFile(pictureFile);
-        }
-        // 去拍照
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-    }
-
-    /**
-     * 小图模式切割图片
-     * 此方式直接返回截图后的 bitmap，由于内存的限制，返回的图片会比较小
-     */
-    public void startSmallPhotoZoom(Uri uri) {
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(uri, "image/*");
-        intent.putExtra("crop", "true");
-        intent.putExtra("aspectX", 1); // 裁剪框比例
-        intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", 300); // 输出图片大小
-        intent.putExtra("outputY", 300);
-        intent.putExtra("scale", true);
-        intent.putExtra("return-data", true);
-        startActivityForResult(intent, REQUEST_SMALL_IMAGE_CUTTING);
-    }
-
-    /**
-     * 大图模式切割图片
-     * 直接创建一个文件将切割后的图片写入
-     */
-    public void startBigPhotoZoom(Uri uri) {
-        // 创建大图文件夹
-        Uri imageUri = null;
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            String storage = Environment.getExternalStorageDirectory().getPath();
-            File dirFile = new File(storage + "/bigIcon");
-            if (!dirFile.exists()) {
-                if (!dirFile.mkdirs()) {
-                    Log.e("TAG", "文件夹创建失败");
-                } else {
-                    Log.e("TAG", "文件夹创建成功");
-                }
-            }
-            File file = new File(dirFile, System.currentTimeMillis() + ".jpg");
-            imageUri = Uri.fromFile(file);
-        }
-        // 开始切割
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(uri, "image/*");
-        intent.putExtra("crop", "true");
-        intent.putExtra("aspectX", 1); // 裁剪框比例
-        intent.putExtra("aspectY", 1);
-        intent.putExtra("outputX", 600); // 输出图片大小
-        intent.putExtra("outputY", 600);
-        intent.putExtra("scale", true);
-        intent.putExtra("return-data", false); // 不直接返回数据
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri); // 返回一个文件
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-        startActivityForResult(intent, REQUEST_BIG_IMAGE_CUTTING);
-    }
-
-    /**
-     * 小图模式中，保存图片后，设置到视图中
-     */
-    private void setPicToView(Intent data) {
-        Bundle extras = data.getExtras();
-        if (extras != null) {
-            Bitmap photo = extras.getParcelable("data"); // 直接获得内存中保存的 bitmap
-            // 创建 smallIcon 文件夹
-            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                String storage = Environment.getExternalStorageDirectory().getPath();
-                File dirFile = new File(storage + "/smallIcon");
-                if (!dirFile.exists()) {
-                    if (!dirFile.mkdirs()) {
-                        Log.e("TAG", "文件夹创建失败");
-                    } else {
-                        Log.e("TAG", "文件夹创建成功");
-                    }
-                }
-                File file = new File(dirFile, System.currentTimeMillis() + ".jpg");
-                // 保存图片
-                FileOutputStream outputStream = null;
-                try {
-                    outputStream = new FileOutputStream(file);
-                    photo.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-                    outputStream.flush();
-                    outputStream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            // 在视图中显示图片
-            binding.userIcon.setImageBitmap(photo);
-        }
-    }
 }
